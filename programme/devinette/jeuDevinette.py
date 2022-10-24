d10bca0e1aca95728d35a97a35b2259493497536
delmat238 <matdelhomel@gmail.com>
2022-10-24 19:58:36 +0200

    deuxieme tour presque finis fut juste tester

--- programme/devinette/jeuDevinette.py
+++ programme/devinette/jeuDevinette.py
@@ -22,7 +22,7 @@ def jeuDevinette(joueur1: Joueur, joueur2: Joueur):
 
     while nbATrouver <= 0 or nbATrouver >= born:
         try:
-            nbATrouver = int(getpass.getpass("Entrez le nombre que " + joueur2.pseudo + " doit trouver entre 0 et", born, ": "))
+            nbATrouver = int(getpass.getpass("Entrez le nombre que " + joueur2.pseudo + " doit trouver entre 0 et " + str(born) + " : "))
             break
         except ValueError:
             print("Erreur de saisie")
@@ -31,7 +31,7 @@ def jeuDevinette(joueur1: Joueur, joueur2: Joueur):
         proposition = saisieInt(joueur2.pseudo + " devine le nombre : ", "Erreur de saisie")
         comptJ1 += 1
 
-        print(joueur2.pseudo, "à vous de jouer")
+        print(joueur1.pseudo, "à vous de jouer")
         print(proposition, "est :")
         print("1. Plus grand")
         print("2. Plus petit")
@@ -40,14 +40,53 @@ def jeuDevinette(joueur1: Joueur, joueur2: Joueur):
         choix = saisieInt(joueur1.pseudo + " faites votre choix : ", "Erreur de saisie")
 
         if choix == 1 and proposition < nbATrouver:
+            print(joueur1.pseudo, "n'essaye pas de tricher")
+        if choix == 2 and proposition > nbATrouver:
+            print(joueur1.pseudo, "n'essaye pas de tricher")
+        if choix != 3 and proposition == nbATrouver:
+            print(joueur1.pseudo, "n'essaye pas de tricher")
+            choix = 3
+        if choix == 3 and proposition != nbATrouver:
+            print("Fait attention", joueur1.pseudo)
+
+    print("Bien joué", joueur1.pseudo, "vous avez gagné en", comptJ1, "coups")
+
+    print(joueur1.pseudo, "à vous de deviner le nombre que", joueur2.pseudo, "a choisi")
+
+    while nbATrouver <= 0 or nbATrouver >= born:
+        try:
+            nbATrouver = int(getpass.getpass("Entrez le nombre que " + joueur1.pseudo + " doit trouver entre 0 et " + str(born) + " : "))
+            break
+        except ValueError:
+            print("Erreur de saisie")
+
+    choix = 0
+    while choix != 3:
+        proposition = saisieInt(joueur1.pseudo + " devine le nombre : ", "Erreur de saisie")
+        comptJ1 += 1
+
+        print(joueur2.pseudo, "à vous de jouer")
+        print(proposition, "est :")
+        print("1. Plus grand")
+        print("2. Plus petit")
+        print("3. Egal")
+
+        choix = saisieInt(joueur2.pseudo + " faites votre choix : ", "Erreur de saisie")
+
+        if choix == 1 and proposition < nbATrouver:
             print(joueur2.pseudo, "n'essaye pas de tricher")
         if choix == 2 and proposition > nbATrouver:
             print(joueur2.pseudo, "n'essaye pas de tricher")
         if choix != 3 and proposition == nbATrouver:
             print(joueur2.pseudo, "n'essaye pas de tricher")
             choix = 3
+        if choix == 3 and proposition != nbATrouver:
+            print("Fait attention", joueur1.pseudo)
 
-    print("Bien joué", joueur1.pseudo, "vous avez gagné en", comptJ1, "coups")
+
+
+
+
 
 
 
