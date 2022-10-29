797037e68b031be6e24758f05bba5ecdb3be87ee
WatzTheEngineer <watztheengineer@gmail.com>
2022-10-29 22:26:18 +0200

    Début gestion des scores

--- programme/joueur/joueur.py
+++ programme/joueur/joueur.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Joueur:
     pseudo: str
     scoreDevinette: int
@@ -6,9 +9,21 @@ class Joueur:
 
     def __init__(self, pseudo: str):
         self.pseudo = pseudo
-        self.scoreDevinette = 0
-        self.scoreAllumette = 0
-        self.scoreMorpion = 0
+        with open("programme/joueur/scores.json", "r") as r_score_file:
+            data = json.load(r_score_file) 
+            with open("programme/joueur/scores.json", "w") as w_score_file:
+                if not pseudo in data['players']:
+                    data['players'][pseudo] = {
+                        "allumette": 0,
+                        "devinette": 0,
+                        "morpion": 0,
+                        "puissance 4": 0
+                    }
+                json.dump(data, w_score_file, indent=4)
+            self.scoreDevinette = data['players'][pseudo]["devinette"]
+            self.scoreAllumette = data['players'][pseudo]["allumette"]
+            self.scoreMorpion = data['players'][pseudo]["morpion"]
+            self.scoreP4 = data['players'][pseudo]["puissance 4"]
 
     def afficherScore(self):
         self.afficherScoreDevinette()
---
+++ programme/utile/score.py
@@ -0,0 +1,46 @@
+import json
+
+from programme.joueur.joueur import Joueur
+
+
+def setScore(score: int, joueur: Joueur, nomjeu: str):
+    """Permet de définir le nouveau score d'un joueur
+
+    Args:
+        score (int): nouveau score du joueur
+        joueur (Joueur): Joueur concerné
+    """
+
+    with open("programme/joueur/scores.json", "r") as r_score_file:
+        data = json.load(r_score_file)
+        with open("programme/joueur/scores.json", "w") as w_score_file:
+            if joueur.pseudo in data['players']:
+                data['players'][joueur.pseudo][nomjeu] = score
+            else:
+                data['players'][joueur.pseudo] = {
+                    "allumette": 0,
+                    "devinette": 0,
+                    "morpion": 0,
+                    "puissance 4": 0
+                }
+                data['players'][joueur.pseudo][nomjeu] = score
+            json.dump(data, w_score_file, indent=4)
+
+
+def incrementScore(joueur: Joueur, nomjeu: str):
+    """Use autoDocstring extension to complete"""
+    with open("programme/joueur/scores.json", "r") as r_score_file:
+        data = json.load(r_score_file)
+        with open("programme/joueur/scores.json", "w") as w_score_file:
+            if joueur.pseudo in data['players']:
+                data['players'][joueur.pseudo][nomjeu] = int(
+                    data['players'][joueur.pseudo][nomjeu])+1
+            else:
+                data['players'][joueur.pseudo] = {
+                    "allumette": 0,
+                    "devinette": 0,
+                    "morpion": 0,
+                    "puissance 4": 0
+                }
+                data['players'][joueur.pseudo][nomjeu] = 1
+            json.dump(data, w_score_file, indent=4)
